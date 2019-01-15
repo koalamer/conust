c64141ccae74f17df408a3d4f9c7cc4d3d1ca552
Balazs Fodor <fodorbalazs@freemail.hu>
2019-01-16 00:09:20 +0100

    Decoding

--- README.md
+++ README.md
@@ -5,18 +5,18 @@ A utility to transform numbers into alphabetically sortable strings and vice ver
 The there are two format options for the transformed strings, one uses the decimal (Base(10)) representation of the number, the other it's Base(36) representation.
 Trailing zeros of the integral part of each number are encoded in one byte, and the leading zeros of the fractional part are encoded similarly. This means, that if your numbers typically contain lots of zeros, you probably should use the Base(10) variant. If, however, your numbers are of arbitrary value, you might be able to save some space by using the Base(36) variant.
 
-The encoded values can store numbers with up to 36 digits in their respective bases for both the integral and the fractional part. Both variants can hold the full range of an int32 or int64 values with one exception: since the minimum value of both int32 (-2 147 483 648) and int64 (-9 223 372 036 854 775 808) has an absolute value which is bigger by one than the maximum value that these types can represent, trying to encode them causes an error.
+The encoded values can store numbers with up to 35 digits in their respective bases for both the integral and the fractional part. Both variants can hold the full range of an int32 or int64 values with one exception: since the minimum value of both int32 (-2 147 483 648) and int64 (-9 223 372 036 854 775 808) has an absolute value which is bigger by one than the maximum value that these types can represent, trying to encode them causes an error.
 If there is a chance you may come across these minimum values, do check the input values before sending them to the encoder.  
 
 ## Encoded Format Description
 
-The zero value is transformed to simply the zero character (lowercase letter "o"). This means that regardless wether the input was "0", "0.0","+0", "-0.0" or any other representation of zero, the output will simply be the zero character.
+The zero value is transformed to simply the zero character "5". This means that regardless wether the input was "0", "0.0","+0", "-0.0" or any other representation of zero, the output will simply be the zero character.
 
-The first character of the output will be "p" for positive numbers, and "n" for negative numbers.
+The first character of the output will be "9" for positive numbers, and "0" for negative numbers.
 
 The length of the number is encoded in a Base(36) digit followed by the number itself, omitting the trailing zeros.
 
-In case of a negative numbers, all digits are swapped for their corresponding pair counting from the end of the digi alphabet. E.g. in Base(10) a "2" becomes "7" and vice vera, in Base(36) "1" bezomes "y" and vice versa. Furthermore, a "~" (tilde character) is appended to the output.
+In case of a negative numbers, all digits are swapped for their corresponding pair counting from the end of the digi alphabet. E.g. in Base(10) a "2" becomes "7" and vice vera, in Base(36) "1" becomes "y" and vice versa. Furthermore, a "~" (tilde character) is appended to the output.
 
 The fractional part is separated from the integral part by "." (period) in case of positive numbers, and by a "~" (tilde) for negative ones.
 When encoding fractional numbers, you have to define the number of decimal digits to keep. The input value will be rounded accordingly before processing.
--- conust.go
+++ conust.go
@@ -25,14 +25,16 @@ const digit9 byte = '9'
 const digitA byte = 'a'
 const digitZ byte = 'z'
 
-const signPositive byte = 'p'
-const signNegative byte = 'n'
-const zeroOutput = "o"
+const signPositive byte = '9'
+const signNegative byte = '0'
+const zeroOutput = "5"
 
 const builderInitialCap = 7
 
 const trailing0 = "0"
 
+// TODO are these decimal points ok?
+const positiveIntegerTerminator byte = '.'
 const negativeIntegerTerminator byte = '~'
 
 // B10FromI32 encodes int32 into sortable string using decimal digits
@@ -40,14 +42,18 @@ func B10FromI32(i int32) (s string) {
 	if i == 0 {
 		return zeroOutput
 	}
-	return decFromIntString(int32Preproc(i))
+	return b10FromIntString(int32Preproc(i))
 }
 
 // B10ToI32 decodes string into int32
 // Successfulness of the decoding is signalled by the second return value. A failure is possible when the encoded number is out of the range of the int32 type.
 func B10ToI32(s string) (i int32, ok bool) {
-	// TODO
-	return 0, false
+	if s == zeroOutput {
+		return 0, true
+	}
+	intPart, _ := decodeStrings(s, true, flipDigit10)
+	result, err := strconv.ParseInt(intPart, 10, 32)
+	return int32(result), (err == nil)
 }
 
 // B10FromI64 encodes int64 into sortable string using decimal digits
@@ -55,7 +61,18 @@ func B10FromI64(i int64) (s string) {
 	if i == 0 {
 		return zeroOutput
 	}
-	return decFromIntString(int64Preproc(i))
+	return b10FromIntString(int64Preproc(i))
+}
+
+// B10ToI64 decodes string into int32
+// Successfulness of the decoding is signalled by the second return value. A failure is possible when the encoded number is out of the range of the int32 type.
+func B10ToI64(s string) (i int64, ok bool) {
+	if s == zeroOutput {
+		return 0, true
+	}
+	intPart, _ := decodeStrings(s, true, flipDigit10)
+	result, err := strconv.ParseInt(intPart, 10, 64)
+	return result, (err == nil)
 }
 
 // B36FromI32 encodes int32 into sortable string using Base(36) digits
@@ -86,8 +103,24 @@ func B36FromI64(i int64) (s string) {
 // B36ToI32 decodes string into int32
 // Successfulness of the decoding is signalled by the second return value. A failure is possible when the encoded number is out of the range of the int32 type.
 func B36ToI32(s string) (i int32, ok bool) {
-	// TODO
-	return 0, false
+	if s == zeroOutput {
+		return 0, true
+	}
+	intPart, _ := decodeStrings(s, true, flipDigit36)
+	result, err := strconv.ParseInt(intPart, 36, 32)
+	return int32(result), (err == nil)
+}
+
+// B36ToI64 decodes string into int32
+// Successfulness of the decoding is signalled by the second return value. A failure is possible when the encoded number is out of the range of the int32 type.
+func B36ToI64(s string) (i int64, ok bool) {
+	if s == zeroOutput {
+		return 0, true
+	}
+	intPart, _ := decodeStrings(s, true, flipDigit36)
+	result, err := strconv.ParseInt(intPart, 36, 64)
+	fmt.Println("b36 decode raw", intPart, "error", err)
+	return result, (err == nil)
 }
 
 func intStringToB10(b *strings.Builder, positive bool, number string) {
@@ -125,22 +158,22 @@ func int32Preproc(i int32) (positive bool, number string) {
 	return false, fmt.Sprintf("%d", i*-1)
 }
 
-func int64Preproc(i int64) (positive bool, number string) {
+func int64Preproc(i int64) (positive bool, absNumber string) {
 	if i > 0 {
 		return true, fmt.Sprintf("%d", i)
 	}
 	return false, fmt.Sprintf("%d", i*-1)
 }
 
-func decFromIntString(positive bool, number string) string {
+func b10FromIntString(positive bool, absNumber string) string {
 	var b strings.Builder
 	b.Grow(builderInitialCap)
 	if positive {
 		b.WriteByte(signPositive)
-		intStringToB10(&b, true, number)
+		intStringToB10(&b, true, absNumber)
 	} else {
 		b.WriteByte(signNegative)
-		intStringToB10(&b, false, number)
+		intStringToB10(&b, false, absNumber)
 	}
 	return b.String()
 }
@@ -162,9 +195,9 @@ func digitToInt(digit byte) int {
 
 func reversedDigitToInt(digit byte) int {
 	if digit < digitA {
-		return int(digitZ - digit)
+		return 26 + int(digit9-digit)
 	}
-	return 26 + int(digit9-digit)
+	return int(digitZ - digit)
 }
 
 func intToDigit36(i int) byte {
@@ -177,4 +210,63 @@ func intToReversedDigit10(i int) byte {
 
 func intToReversedDigit36(i int) byte {
 	return digits36Reversed[i]
+}
+
+func decodeStrings(s string, intOnly bool, flipDigit func(byte) byte) (integral string, fractional string) {
+	isPositive := (s[0] == signPositive)
+	var terminator byte
+	var intLength int
+
+	if isPositive {
+		terminator = positiveIntegerTerminator
+		intLength = digitToInt(s[1])
+	} else {
+		terminator = negativeIntegerTerminator
+		intLength = reversedDigitToInt(s[1])
+	}
+
+	var b strings.Builder
+	b.Grow(intLength + 1)
+
+	if isPositive {
+		// b.WriteByte('+')
+	} else {
+		b.WriteByte('-')
+	}
+
+	terminatorPos := strings.IndexByte(s, terminator)
+	if terminatorPos > 1 {
+		if isPositive {
+			b.WriteString(s[2:terminatorPos])
+		} else {
+			for i := 2; i < terminatorPos; i++ {
+				b.WriteByte(flipDigit(s[i]))
+			}
+		}
+	} else {
+		if isPositive {
+			b.WriteString(s[2:])
+		} else {
+			for i := 2; i < len(s); i++ {
+				b.WriteByte(flipDigit(s[i]))
+			}
+		}
+	}
+	if bLength := b.Len(); bLength <= intLength {
+		for i := bLength; i < intLength; i++ {
+			b.WriteByte('0')
+		}
+	}
+	if intOnly || terminatorPos < 0 || terminatorPos == len(s)-1 {
+		return b.String(), ""
+	}
+
+	integral = b.String()
+	b.Reset()
+	leadingZeroes := reversedDigitToInt(s[terminatorPos+1])
+	for i := 0; i < leadingZeroes; i++ {
+		b.WriteByte('0')
+	}
+	b.WriteString(s[terminatorPos+2:])
+	return integral, b.String()
 }
