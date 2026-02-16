2a95ea06d7d131d6bf6c0054832fbe73f0d73350
Fodor Balázs <fodorbali@outlook.com>
2026-02-16 08:16:09 +0100

    WIP segment scanner

--- segment.go
+++ segment.go
@@ -13,11 +13,6 @@ const (
 
 type segmentTypeValue uint8
 
-const (
-	segmentTypeText segmentTypeValue = iota
-	segmentTypeNumber
-)
-
 type segmentScanner struct {
 	input string
 	// config
@@ -65,7 +60,7 @@ func (s *segmentScanner) Reset(input string) {
 	s.containsSingleNumber = true
 }
 
-func (s *segmentScanner) Next() (string, segmentTypeValue) {
+func (s *segmentScanner) Next() (segment string, isNumber bool) {
 	startPos := s.headPos
 	digitPos := s.headPos
 	inputLength := len(s.input)
@@ -81,11 +76,10 @@ func (s *segmentScanner) Next() (string, segmentTypeValue) {
 	// no digit was found
 	if digitPos >= inputLength {
 		s.headPos = digitPos
-		return s.input[startPos:digitPos], segmentTypeText
+		return s.input[startPos:digitPos], false
 	}
 
-	textEndPos := digitPos
-	minusSignFound := false
+	numberSegmentStartPos := digitPos
 
 	// adjust segment end index according to sign detection setting
 	if digitPos > startPos {
@@ -94,116 +88,58 @@ func (s *segmentScanner) Next() (string, segmentTypeValue) {
 
 		if (byteBeforeDigit == plusByte && s.usePlusSign) ||
 			(byteBeforeDigit == minusByte && s.useMinusSign) {
-			textEndPos = indexBeforeDigit
-			minusSignFound = (byteBeforeDigit == minusByte)
+			numberSegmentStartPos = indexBeforeDigit
 		}
 	}
 
 	// there is some text before the number (and optional sign character)
-	if textEndPos > startPos {
-		s.headPos = textEndPos
-		return s.input[startPos:textEndPos], segmentTypeText
+	if numberSegmentStartPos > startPos {
+		s.headPos = numberSegmentStartPos
+		return s.input[startPos:numberSegmentStartPos], false
 	}
 
-	// the segment is a number segment
+	// the segment is a number segment, find the end
+	useDecSep := s.useDecimalSeparator
+	useThSep := s.useThousandSeparator
 
-	// TODO parse number
+	for i := digitPos + 1; i < inputLength; i++ {
+		b := s.input[i]
+		if s.digitTester.isDigit(b) {
+			continue
+		}
 
-	return "", segmentTypeText
+		if useThSep && b == s.thousandSeparator {
+			afterSeparatorIndex := i + 1
+			// separator is not followed by a digit, so the number ended before it
+			if afterSeparatorIndex >= inputLength || !s.digitTester.isDigit(s.input[afterSeparatorIndex]) {
+				s.headPos = i
+				return s.input[numberSegmentStartPos:i], true
+			}
+
+			i = afterSeparatorIndex + 1
+			continue
+		}
+
+		if useDecSep && b == s.decimalSeparator {
+			afterSeparatorIndex := i + 1
+			// separator is not followed by a digit, so the number ended before it
+			if afterSeparatorIndex >= inputLength || !s.digitTester.isDigit(s.input[afterSeparatorIndex]) {
+				s.headPos = i
+				return s.input[numberSegmentStartPos:i], true
+			}
+
+			useThSep = false
+			useDecSep = false
+
+			i = afterSeparatorIndex + 1
+			continue
+		}
+
+		return s.input[numberSegmentStartPos:i], true
+	}
+
+	return s.input[numberSegmentStartPos:], true
 }
 
 func (s *segmentScanner) findFirstDigit() {
 }
-
-func (s *segmentScanner) xbyteType(b byte) byteTypeValue {
-	if s.digitTester.isDigit(b) {
-		return byteTypeDigit
-	}
-
-	if b == minusByte {
-		if s.useMinusSign {
-			return byteTypeMinus
-		}
-		return byteTypeText
-	}
-
-	if b == plusByte {
-		if s.usePlusSign {
-			return byteTypePlus
-		}
-		return byteTypeText
-	}
-
-	if b == s.thousandSeparator {
-		if s.useThousandSeparator {
-			return byteTypeThousandSeparator
-		}
-		return byteTypeText
-	}
-
-	if b == s.decimalSeparator {
-		if s.useDecimalSeparator {
-			return byteTypeDecimalSeparator
-		}
-		return byteTypeText
-	}
-
-	return byteTypeText
-}
-
-/*
-func (s *segmentScanner) segmentTypeAtPos(pos int, previousSegmentType segmentTypeValue) segmentTypeValue {
-	inputLen := len(s.input)
-
-	if inputLen <= pos {
-		return segmentTypeText
-	}
-
-	firstByteType := s.byteType(s.input[pos])
-
-	if firstByteType == byteTypeText {
-		return segmentTypeText
-	}
-
-	if firstByteType == byteTypeDigit {
-		return segmentTypeNumber
-	}
-
-	if previousSegmentType == segmentTypeText {
-		if firstByteType == byteTypePlus || firstByteType == byteTypeMinus {
-			pos += 1
-
-			if inputLen <= pos {
-				return segmentTypeText
-			}
-
-			secondByteType := s.byteType(s.input[pos])
-
-			if secondByteType == byteTypeDigit {
-				return segmentTypeNumber
-			}
-
-			return segmentTypeText
-		}
-	}
-
-	// previousSegmentType == segmentTypeNumber
-	if firstByteType == byteTypeThousandSeparator || firstByteType == byteTypeDecimalSeparator {
-		pos += 1
-
-		if inputLen <= pos {
-			return segmentTypeText
-		}
-
-		secondByteType := s.byteType(s.input[pos])
-
-		if secondByteType == byteTypeDigit {
-			return segmentTypeNumber
-		}
-
-		return segmentTypeText
-	}
-
-	return segmentTypeText
-}
-*/
